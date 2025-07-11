34a39f0729762aa83aa7b68fbef14d96c6519e2b
isaaccorley <22203655+isaaccorley@users.noreply.github.com>
2025-07-11 02:22:45 +0000

    reorganize to torch directories

---
+++ stac_model/torch/export.py
@@ -0,0 +1,186 @@
+import glob
+import inspect
+import logging
+import os
+import tempfile
+import zipfile
+from collections.abc import Sequence
+
+import torch
+import yaml
+from torch.export.dynamic_shapes import Dim
+from torch.export.pt2_archive._package import package_pt2
+
+import stac_model.torch.export as export
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+def package_model_and_transforms(
+    output_file: str,
+    model_program: export.export.ExportedProgram,
+    transforms_program: export.export.ExportedProgram | None = None,
+    metadata_path: str | None = None,
+    aoti_compile_and_package: bool = False,
+) -> None:
+    """Packages a model and its transforms AOTI exported programs into a single archive file.
+
+    Args:
+        output_file (str): The path to the output archive file.
+        model_program (torch.export.ExportedProgram): The exported model program.
+        transforms_program (torch.export.ExportedProgram | None): The exported transforms program.
+        metadata_path (str | None): Path to the YAML file containing model metadata.
+        aoti_compile_and_package (bool): Whether to compile and package the model and transforms using AOTI. Defaults to True.
+    """
+    # Load metadata file
+    if metadata_path is not None:
+        with open(metadata_path) as f:
+            metadata = yaml.safe_load(f)
+        extra_files = {"metadata": yaml.dump(metadata)}
+    else:
+        extra_files = None
+
+    # Package programs only
+    if not aoti_compile_and_package:
+        programs = {"model": model_program}
+        if transforms_program is not None:
+            programs["transforms"] = transforms_program
+            package_pt2(
+                f=output_file,
+                exported_programs=programs,
+                extra_files=extra_files,
+            )
+
+    # Attempt to AOTI compile and package
+    else:
+        with (
+            tempfile.TemporaryDirectory() as archive_tmpdir,
+            tempfile.TemporaryDirectory() as model_tmpdir,
+            tempfile.TemporaryDirectory() as transforms_tmpdir,
+        ):
+            # Package and extract transforms from pt2 archive
+            model_path = export._inductor.aoti_compile_and_package(
+                model_program, package_path=os.path.join(archive_tmpdir, "model.pt2")
+            )
+
+            # Extract model files from archive
+            with zipfile.ZipFile(model_path, "r") as zip_ref:
+                zip_ref.extractall(model_tmpdir)
+
+            model_files = [
+                f
+                for f in glob.glob(os.path.join(model_tmpdir, "**"), recursive=True)
+                if os.path.isfile(f)
+            ]
+            aoti_files = {"model": model_files}
+
+            if transforms_program is not None:
+                # Package and extract transforms from pt2 archive
+                transforms_path = export._inductor.aoti_compile_and_package(
+                    transforms_program,
+                    package_path=os.path.join(archive_tmpdir, "transforms.pt2"),
+                )
+
+                with zipfile.ZipFile(transforms_path, "r") as zip_ref:
+                    zip_ref.extractall(transforms_tmpdir)
+
+                transforms_files = [
+                    f
+                    for f in glob.glob(os.path.join(transforms_tmpdir, "**"), recursive=True)
+                    if os.path.isfile(f)
+                ]
+                aoti_files["transforms"] = transforms_files
+
+            # Package model, transforms, and metadata together
+            package_pt2(
+                f=output_file,
+                aoti_files=aoti_files,
+                extra_files=extra_files,
+            )
+
+
+@export.no_grad()
+def export_model_and_transforms(
+    model: torch.nn.Module,
+    transforms: torch.nn.Module,
+    input_shape: Sequence[int],
+    device: export.device,
+) -> tuple[export.export.ExportedProgram, export.export.ExportedProgram]:
+    """Exports a model and its transforms to programs.
+
+    Args:
+        model (torch.nn.Module): The model to export.
+        transforms (torch.nn.Module): The transforms to export.
+        input_shape (Sequence[int]): The shape of the input tensor, where -1 indicates a dynamic dimension.
+        device (torch.device): The device to export the model and transforms to.
+
+    Returns:
+        tuple[torch.export.ExportedProgram, torch.export.ExportedProgram]:
+            A tuple containing the exported model program and transforms program.
+    """
+    model.eval()
+    transforms.eval()
+    model = model.to(device)
+    transforms = transforms.to(device)
+
+    # Construct example inputs and dims
+    example_inputs = _create_example_input_from_shape(input_shape).to(device)
+    dims = tuple(Dim.AUTO if dim == -1 else dim for dim in input_shape)
+    logger.debug(msg=f"Exporting with dims: {dims}")  # noqa: G004
+    logger.debug(f"Example input shape: {example_inputs.shape}")  # noqa: G004
+
+    # Extract argument names for the model and transforms forward methods
+    model_arg = next(iter(inspect.signature(model.forward).parameters))
+    transforms_arg = next(iter(inspect.signature(transforms.forward).parameters))
+
+    # Export model and transforms
+    model_program = export.export.export(
+        mod=model, args=(example_inputs,), dynamic_shapes={model_arg: dims}
+    )
+    transforms_program = export.export.export(
+        mod=transforms, args=(example_inputs,), dynamic_shapes={transforms_arg: dims}
+    )
+    return model_program, transforms_program
+
+
+def _create_example_input_from_shape(input_shape: Sequence[int]) -> export.Tensor:
+    """Creates an example input tensor based on the provided input shape.
+
+    Args:
+        input_shape (Sequence[int]): The shape of the input tensor.
+
+    Returns:
+        torch.Tensor: A tensor filled with random values, shaped according to the input_shape.
+
+    Raises:
+        ValueError: If the channel dimension is dynamic (-1).
+    """
+    # Handle dynamic dimensions (-1) by replacing them with a fixed value
+    shape = []
+
+    # Batch dimension
+    if input_shape[0] == -1:
+        shape.append(2)
+    else:
+        shape.append(input_shape[0])
+
+    # Channel dimension should always be set
+    if input_shape[1] != -1:
+        shape.append(input_shape[1])
+    else:
+        raise ValueError("Channel dimension cannot be dynamic (-1) for input shape.")
+
+    # Height dimension
+    if input_shape[2] == -1:
+        shape.append(224)
+    else:
+        shape.append(input_shape[2])
+
+    # Width dimension
+    if input_shape[3] == -1:
+        shape.append(224)
+    else:
+        shape.append(input_shape[3])
+
+    return export.randn(*shape, requires_grad=False)
---
+++ tests/torch/test_export.py
@@ -0,0 +1,69 @@
+import os
+from pathlib import Path
+
+import pytest
+import torch
+import torchvision.transforms.v2 as T
+import yaml
+from torch.export.pt2_archive._package import load_pt2
+from torchgeo.models import Unet_Weights, unet
+
+from stac_model.torch.export import (
+    export_model_and_transforms,
+    package_model_and_transforms,
+)
+
+
+def export_model(tmpdir: Path, device: str, aoti_compile_and_package: bool) -> None:
+    device = torch.device(device)
+    input_shape = (-1, 8, -1, -1)
+    archive_path = os.path.join(tmpdir, "model.pt2")
+    metadata_path = os.path.join("tests", "torch", "ftw-metadata.yaml")
+    weights = Unet_Weights.SENTINEL2_3CLASS_FTW
+    transforms = torch.nn.Sequential(*[T.Resize((256, 256)), T.Normalize(mean=[0.0], std=[3000.0])])
+    model = unet(weights=weights)
+    model_program, transforms_program = export_model_and_transforms(
+        model=model,
+        transforms=transforms,
+        input_shape=input_shape,
+        device=device,
+    )
+    package_model_and_transforms(
+        output_file=archive_path,
+        model_program=model_program,
+        transforms_program=transforms_program,
+        metadata_path=metadata_path,
+        aoti_compile_and_package=aoti_compile_and_package,
+    )
+
+    # Validate that pt2 is loadable
+    pt2 = load_pt2(archive_path)
+    if pt2.aoti_runners == {}:
+        model = pt2.aoti_runners["model"]
+        transforms = pt2.aoti_runners["transforms"]
+    else:
+        model = pt2.exported_programs["model"].module()
+        transforms = pt2.exported_programs["transforms"].module()
+
+    # Validate transforms are usable
+    x = torch.randn(1, 8, 128, 128, device=device, requires_grad=False)
+    transforms(x)
+
+    # Validate model is usable
+    x = torch.randn(1, 8, 128, 128, device=device, requires_grad=False)
+    model(x)
+
+    # Validate metadata is valid yaml
+    metadata = pt2.extra_files["metadata"]
+    yaml.safe_load(metadata)
+
+
+@pytest.mark.parametrize("aoti_compile_and_package", [True, False])
+def test_ftw_export_cpu(tmpdir: Path, aoti_compile_and_package: bool) -> None:
+    export_model(tmpdir, "cpu", aoti_compile_and_package=aoti_compile_and_package)
+
+
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA is not available")
+@pytest.mark.parametrize("aoti_compile_and_package", [True, False])
+def test_ftw_export_cuda(tmpdir: Path, aoti_compile_and_package: bool) -> None:
+    export_model(tmpdir, "cuda", aoti_compile_and_package=aoti_compile_and_package)
