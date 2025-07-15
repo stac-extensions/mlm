dfe4e4c1334d0ebfb4bdff5629ea21234ee62787
isaaccorley <22203655+isaaccorley@users.noreply.github.com>
2025-07-15 23:19:29 +0000

    use  and properties fields in example

--- tests/torch/test_metadata.py
+++ tests/torch/test_metadata.py
@@ -11,4 +11,4 @@ from stac_model.schema import MLModelProperties
     indirect=True,
 )
 def test_mlm_metadata_only_yaml_validation(mlm_example):
-    MLModelProperties.model_validate(mlm_example)
+    MLModelProperties.model_validate(mlm_example["properties"])
