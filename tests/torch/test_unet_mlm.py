9d5298c4cfd4d4a2c5d39a7e3061e6b244f0f817
Nazim Azeli <nazim.azeli@crim.ca>
2025-08-20 13:25:18 -0400

    move test

--- tests/torch/test_unet_mlm.py
+++ tests/torch/test_unet_mlm.py
@@ -9,13 +9,12 @@ from stac_model.examples import unet_mlm
 
 @pytest.mark.skipif(sys.version_info < (3, 11), reason="Requires Python 3.11+")
 def test_unet_mlm_matches_example_json():
-
     try:
         item = unet_mlm().item.to_dict()
     except ModuleNotFoundError as e:
         if e.name == "torchgeo":
             pytest.skip("torchgeo is not installed")
-        raise 
+        raise
 
     json_path = Path(__file__).resolve().parents[2] / "examples" / "item_pytorch_geo_unet.json"
     with open(json_path, "r", encoding="utf-8") as f:
